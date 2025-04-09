765bb1d75a564035dde7fa5d311e3be083cbf2ef
Peter Haluska <halu55@gmail.com>
2025-04-09 20:57:34 +0200

    Update build folder path

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default [
         input: 'src/indexUmd.ts', // The entry file
         output: [
             {
-                file: 'src/demo/dist/index.umd.js', // UMD bundle for the browser
+                file: 'dist/index.umd.js', // UMD bundle for the browser
                 format: 'umd',
                 name: 'cspConsoleReporting', // Global variable name for the browser
                 sourcemap: true,
@@ -20,7 +20,7 @@ export default [
         input: 'src/indexEsm.ts', // The entry file
         output: [
             {
-                file: 'src/demo/dist/index.esm.js', // ESM bundle for Webpack, Vite, etc.
+                file: 'dist/index.esm.js', // ESM bundle for Webpack, Vite, etc.
                 format: 'esm',
                 sourcemap: true,
             },
