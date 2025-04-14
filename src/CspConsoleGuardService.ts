7f4b5599d01e55695ec799e4cabea055477b1c3a
Peter Haluska <halu55@gmail.com>
2025-04-14 10:57:27 +0200

    Add some comments and disable meaningless stuff

--- src/CspConsoleGuardService.ts
+++ src/CspConsoleGuardService.ts
@@ -1,11 +1,20 @@
 import { Config, initConfig } from "./config/ConfigService";
 import { cspWebGuard } from "./guard/GuardService";
+import { shouldUseEnforceMode } from "./config/ModeService";
 
 type GuardConfig = Config & {
     onGuardInit?(): void;
 };
 
 export function cspConsoleWebGuard({ onGuardInit, policies, mode, reportUri }: GuardConfig): void {
+    if (shouldUseEnforceMode()) {
+        initConfig({ policies, mode, reportUri });
+        cspWebGuard();
+        onGuardInit?.();
+        return;
+    }
+
+    // THIS IS QUESTIONABLE WHERE TO GO WITH IT...
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./service-worker.js')
             .then(() => {
--- src/guard/GuardService.ts
+++ src/guard/GuardService.ts
@@ -1,3 +1,13 @@
+import { shouldUseEnforceMode } from "../config/ModeService";
+
 export function cspWebGuard(): void {
+    if (shouldUseEnforceMode()) {
+        // THIS DOES NOT MAKE ANY SENSE...when this event occurs, browser already sent the report.
+        // The policy violation is not cancellable, is purely informative
+        window.addEventListener('securitypolicyviolation', function(event) {
+            // reportViolation(event.effectiveDirective, event.blockedURI, event.documentURI, event.originalPolicy, event.referrer);
+        });
+        return;
+    }
 
 }
